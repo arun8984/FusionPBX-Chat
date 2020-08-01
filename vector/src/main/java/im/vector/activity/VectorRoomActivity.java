cb21bea1457a6a8c4179fe77e427f784e2968c99
tusharnigam88@gmail.com <tusharnigam88@gmail.com>
2020-08-01 14:37:52 +0530

    fixed

--- vector/src/main/java/im/vector/activity/VectorRoomActivity.java
+++ vector/src/main/java/im/vector/activity/VectorRoomActivity.java
@@ -1740,18 +1740,6 @@ public class VectorRoomActivity extends MXCActionBarActivity implements
         SharedPreferences settings = android.preference.PreferenceManager.getDefaultSharedPreferences(VectorRoomActivity.this);
         long avTimeInMinutes = settings.getLong(PreferencesManager.VIDEO_CALL_TIME, 0);
         SharedPreferences preferences = android.preference.PreferenceManager.getDefaultSharedPreferences(VectorRoomActivity.this);
-        boolean isTrial = preferences.getBoolean(PreferencesManager.IS_TRIAL, false);
-        if (!isTrial && isVideoCall && avTimeInMinutes <= 0) {
-            AlertDialog.Builder b = new AlertDialog.Builder(VectorRoomActivity.this)
-                    .setTitle(getString(R.string.app_name));
-            b.setNeutralButton(R.string.ok, new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    dialog.dismiss();
-                }
-            }).show();
-            return;
-        }
         AlertDialog.Builder builder = new AlertDialog.Builder(VectorRoomActivity.this)
                 .setTitle(R.string.dialog_title_confirmation);
 
